37ee533bd4f887f5da5040f63c34c71efe2f55a1
Matthew Kayne <matthewkayne2004@gmail.com>
2022-02-18 23:10:39 +0000

    cleaned code

--- Hangman.py
+++ Hangman.py
@@ -1,9 +1,5 @@
-#Hangman
-
+import random
 def hangman():
-
-  import random
-
   words=["hello","matthew","table","rain","chair","mystery","liverpool","london","madrid","jeremy","computer","lesson","nice","well","great","awesome","brilliant"]
   stars=[]
   answer=[]
@@ -17,7 +13,6 @@ def hangman():
   rand_word_answer = rand_word
   rand_word=list(rand_word)
   
-  
   flag=False
 
   count=0
@@ -28,16 +23,11 @@ def hangman():
   for i in range(0,y):
     stars.append(x)
   
-  
   while flag == False:
-
     guess=input("Guess a letter: ")
 
     if guess in rand_word:
-
       for i in[i for i,m in enumerate(rand_word) if m == guess]:
-        
-
         answer.append(i)
       
         temp = rand_word[i]
@@ -45,6 +35,7 @@ def hangman():
         stars[i] = temp
 
         temp_string=""
+        
         for letters in range(len(stars)):
           temp_string+=stars[letters]
         
@@ -52,25 +43,18 @@ def hangman():
         if len(rand_word) == len(answer):
           print("Yes you got it right")
           flag=True
-          
-        else:
-          pass
+
       print(temp_string)
             
     else:
-      
       print("nope")
       count+=1
       print("You have", (5-count),"lives remaining")
       
       if count == 5:
-        
         print("The word was",rand_word_answer)
         print("Game Over")
         flag=True
-      
-      else:
-        
-        pass     
+   
 
 hangman()
