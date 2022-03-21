5ad61d5b979be5e524fdad40feadd0d7cc275563
Matthew Kayne <matthewkayne2004@gmail.com>
2022-03-21 10:13:00 +0000

    cleaned code

--- Hangman.py
+++ Hangman.py
@@ -1,60 +1,61 @@
 import random
+
+
 def hangman():
-  words=["hello","matthew","table","rain","chair","mystery","liverpool","london","madrid","jeremy","computer","lesson","nice","well","great","awesome","brilliant"]
-  stars=[]
-  answer=[]
-  rand_word=words[random.randint(0,len(words))]
-  
-  x="*"
-  y=len(rand_word)
+    words = ["hello", "matthew", "table", "rain", "chair", "mystery", "liverpool", "london",
+             "madrid", "jeremy", "computer", "lesson", "nice", "well", "great", "awesome", "brilliant"]
+    stars = []
+    answer = []
+    rand_word = words[random.randint(0, len(words))]
 
-  print("The Word To Guess:",x * y)
-  
-  rand_word_answer = rand_word
-  rand_word=list(rand_word)
-  
-  flag=False
+    x = "*"
+    y = len(rand_word)
 
-  count=0
+    print("The Word To Guess:", x * y)
 
-  stars=[]
-  answer=[]
-  
-  for i in range(0,y):
-    stars.append(x)
-  
-  while flag == False:
-    guess=input("Guess a letter: ")
+    rand_word_answer = rand_word
+    rand_word = list(rand_word)
 
-    if guess in rand_word:
-      for i in[i for i,m in enumerate(rand_word) if m == guess]:
-        answer.append(i)
-      
-        temp = rand_word[i]
-        rand_word[i]=stars[i]
-        stars[i] = temp
+    flag = False
+    count = 0
 
-        temp_string=""
-        
-        for letters in range(len(stars)):
-          temp_string+=stars[letters]
-        
+    stars = []
+    answer = []
 
-        if len(rand_word) == len(answer):
-          print("Yes you got it right")
-          flag=True
+    for i in range(0, y):
+        stars.append(x)
 
-      print(temp_string)
-            
-    else:
-      print("nope")
-      count+=1
-      print("You have", (5-count),"lives remaining")
-      
-      if count == 5:
-        print("The word was",rand_word_answer)
-        print("Game Over")
-        flag=True
-   
+    while flag == False:
+        guess = input("Guess a letter: ")
+
+        if guess in rand_word:
+            for i in [i for i, m in enumerate(rand_word) if m == guess]:
+                answer.append(i)
+
+                temp = rand_word[i]
+                rand_word[i] = stars[i]
+                stars[i] = temp
+
+                temp_string = ""
+
+                for letters in range(len(stars)):
+                    temp_string += stars[letters]
+
+                if len(rand_word) == len(answer):
+                    print("Yes you got it right")
+                    flag = True
+
+            print(temp_string)
+
+        else:
+            print("nope")
+            count += 1
+            print("You have", (5-count), "lives remaining")
+
+            if count == 5:
+                print("The word was", rand_word_answer)
+                print("Game Over")
+                flag = True
+
 
 hangman()
